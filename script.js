a8022db22e7d35f51368753051fea6c417a970b6
charithareddy588-prog <charithareddy588@gmail.com>
2026-06-23 17:09:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// Task 1- Employee Name
+
+// let employeeName=prompt("what is your name")
+// console.log("employee name :", employeeName)
+// document.writeln("Welcome ", employeeName)
+
+// Task 2- Company Entry Confirmation
+
+// let companyEntry=confirm("Are you ready to join today's JavaScript training?")
+// console.log(companyEntry)
+
+// task 3- Customer Greeting
+
+// alert("Welcome to Stackly Solutions")
+// console.log("Customer Entered the Website")
+
+// task 4- Student Details
+
+// let studentName=prompt("Name")
+// let studentAge=prompt("Age")
+// console.log(studentName)
+// console.log(studentAge)
+
+//task 5- Product Price 
+
+// let productPrice=50
+// productPrice=80
+// console.log(productPrice)
+
+//task 6- Login Validation
+
+// let userName=prompt("Enter your user name")
+// if(userName===null || userName==="") {
+    // console.warn("Warning: User name is empty")
+// } else {
+//     console.log("Login Successful: ", userName)
+// }
+
+//task 7- Website Maintenance
+
+// alert("Website is under maintenance")
+// console.error("Error: Website is currently unavailable")
+
+//task 8- Feedback Collection
+
+// let feedback=prompt("How was today's JavaScript Session?")
+// console.log(feedback)
+// document.writeln("Thank you for your feedback!")
+
+//task 9- Profile Information
+
+// let name=prompt("Name")
+// let city=prompt("city")
+// // let favouriteProgrammingLanguage=prompt("favourite programming language")
+// console.log(name)
+// console.log(city)
+//console.log(favouriteProgrammingLanguage)
+
+//task 10- Mini registration form 
+
+let name=prompt("Full Name")
+let email=prompt("Email")
+let mobileNumber=prompt("Mobile Number")
+console.log("===== Registration Successful =====")
+console.log(name)
+console.log(email)
+console.log(mobileNumber)
+alert("Registration Successful")
